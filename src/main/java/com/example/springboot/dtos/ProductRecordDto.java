15f98e07fb49e965f1e9ace982709e42641b1247
Thiago França <thiagofranca.pe1999@gmail.com>
2024-05-09 23:45:18 -0300

    Adicionar funcionalidade de gestão de produtos
    Introduzido um novo conjunto de operações CRUD para gerenciar produtos na aplicação. A atualização inclui a criação de um novo ProductModel, ProductRepository, ProductController, e ProductRecordDto. Também atualizado application.properties para configurar a conexão com o banco de dados e propriedades JPA.

---
+++ src/main/java/com/example/springboot/dtos/ProductRecordDto.java
@@ -0,0 +1,10 @@
+package com.example.springboot.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+
+}
